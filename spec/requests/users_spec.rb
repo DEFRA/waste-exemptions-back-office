60572eb37a5c13caa340f74081f37686e9cfdb7b
Toby Privett <toby@snaplab.co.uk>
2021-10-28 10:39:10 +0100

    Styling: Manage users      (#753)
    https://eaflood.atlassian.net/browse/RUBY-1647
    
    This makes several changes to make the back office user management page more, well, manageable:
    
    - list 100 users per page so the in-browser find feature can be used to search if needed
    - only list active and invited users by default
    - add a toggle to list deactivated users as well

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,21 +4,24 @@ require "rails_helper"
 
 RSpec.describe "Users", type: :request do
   describe "/users" do
-    context "when a system is signed in" do
+    context "when a system user is signed in" do
       let(:user) { create(:user, :system) }
       before(:each) do
         sign_in(user)
       end
 
-      it "renders the index template, returns a 200 response, includes the correct content and displays a list of users" do
-        listed_user = create(:user, email: "aaaaaaaaaaa@example.com")
+      it "renders the index template, returns a 200 response and includes the correct content" do
+        active_user = create(:user, email: "aaaaaaaaaaa@example.com")
+        deactivated_user = create(:user, email: "aaaaaaaaaaa2@example.com", active: false)
 
         get "/users"
 
         expect(response).to render_template(:index)
         expect(response).to have_http_status(200)
         expect(response.body).to include("Manage back office users")
-        expect(response.body).to include(listed_user.email)
+        expect(response.body).to include("Show all users")
+        expect(response.body).to include(active_user.email)
+        expect(response.body).to_not include(deactivated_user.email)
       end
     end
 
@@ -38,6 +41,49 @@ RSpec.describe "Users", type: :request do
     context "when no user is signed in" do
       it "redirects the user to the sign in page" do
         get "/users"
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe "/users/all" do
+    context "when a system user is signed in" do
+      let(:user) { create(:user, :system) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      it "renders the index template, returns a 200 response and includes the correct content" do
+        active_user = create(:user, email: "aaaaaaaaaaa@example.com")
+        deactivated_user = create(:user, email: "aaaaaaaaaaa2@example.com", active: false)
+
+        get "/users/all"
+
+        expect(response).to render_template(:index)
+        expect(response).to have_http_status(200)
+        expect(response.body).to include("Show enabled users only")
+        expect(response.body).to include(active_user.email)
+        expect(response.body).to include(deactivated_user.email)
+      end
+    end
+
+    context "when a non-system user is signed in" do
+      let(:user) { create(:user, :data_agent) }
+      before(:each) do
+        sign_in(user)
+      end
+
+      it "redirects to the permissions error page" do
+        get "/users/all"
+
+        expect(response).to redirect_to("/pages/permission")
+      end
+    end
+
+    context "when no user is signed in" do
+      it "redirects the user to the sign in page" do
+        get "/users/all"
 
         expect(response).to redirect_to(new_user_session_path)
       end
