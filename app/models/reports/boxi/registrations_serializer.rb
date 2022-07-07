6fea3d5fefa4113150632951ebe12d06b1dafe66
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-07-07 17:31:37 +0100

    Map assistance_mode values for boxi registrations export

--- app/models/reports/boxi/registrations_serializer.rb
+++ app/models/reports/boxi/registrations_serializer.rb
@@ -12,6 +12,17 @@ module Reports
       def records_scope
         WasteExemptionsEngine::Registration.all
       end
+
+      def parse_assistance_mode(mode)
+        case mode
+        when "full"
+          "Fully assisted"
+        when "partial"
+          "Partially assisted"
+        else
+          "Unassisted"
+        end
+      end
     end
   end
 end
