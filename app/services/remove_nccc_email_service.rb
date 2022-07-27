a3354af5fa9450a53c265d7fc0302d9e259ba7aa
Rebecca Ransome <beckyrose200@aol.com>
2022-07-27 09:44:59 +0100

    One of task to change the contact email and applicant email to nil if its an NCCC one

--- app/services/remove_nccc_email_service.rb
+++ app/services/remove_nccc_email_service.rb
@@ -2,33 +2,7 @@
 
 class RemoveNcccEmailService < ::WasteExemptionsEngine::BaseService
   def run
-    contact_emails = find_nccc_email_in_contact_email
-    return unless contact_emails.present? && contact_emails.any?
-
-    contact_emails.each do |email|
-      email.contact_email = nil
-      email.save!
-    end
-
-    # @applicant_email.each do |email|
-    #   email.applicant_email = nil
-    #   email.applicant_email.save!
-    # end
+    Registration.where("contact_email ~* ?", "cy.gov.uk").update_all(contact_email: nil)
+    Registration.where("applicant_email ~* ?", "cy.gov.uk").update_all(applicant_email: nil)
   end
-  # Change find by on line 23 to a regular expression , look at batch updates for the database
-
-  private
-
-  def find_nccc_email_in_contact_email
-    WasteExemptionsEngine::Registration.find_by(contact_email: "waste-exemptions@environment-agency.gov.uk")
-  end
-
-  # def find_nccc_email_in_applicant_email
-  #   @applicant_emails ||=
-  #     WasteExemptionsEngine::Registration.find_by(applicant_email: "waste-exemptions@environment-agency.gov.uk")
-  # end
-
-  # def NCCC_email_variations
-  #   @NCCC_email_variations = ["waste-exemptions@environment-agency.gov.uk", "nccc@environment-agency.gov.uk"]
-  # end
 end
