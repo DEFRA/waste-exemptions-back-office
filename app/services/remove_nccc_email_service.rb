dfb3e6a8f19568768e73cd8a06db37b6db266d9a
Rebecca Ransome <beckyrose200@aol.com>
2022-08-02 09:23:27 +0100

    Adding specs for removal service and different email variations

--- app/services/remove_nccc_email_service.rb
+++ app/services/remove_nccc_email_service.rb
@@ -1,8 +1,35 @@
 # frozen_string_literal: true
 
+# rubocop:disable Layout/LineLength
 class RemoveNcccEmailService < ::WasteExemptionsEngine::BaseService
+
+  # https://rubular.com/r/LPHA79nxD5k2ff
+  # rubocop:disable Style/StringLiterals
+  WASTE_EXEMPTIONS_EMAIL_VARIATIONS = '^(w|a)[qaste]?\w{2,}(-?(e|-)|)(.?e)\w{7,}@(e|a|n)\w{5,17}(.|-)(ag?(ency|ancy|ncy)|gov).(gov.uk|uk)'
+
+  EXEMPTION_EMAIL_VARIATIONS = '^e(nquiries|xcemption?(s|)|xceptions|xemptions)@environment-agency.gov.uk'
+
+  NCCC_EMAIL_VARIATIONS = '^nccc(-?[cew]\w{4,}(-\w{10,}@|@)|@)enviro\w{4,}-agency.gov.uk'
+  # rubocop:enable Style/StringLiterals
+
   def run
-    Registration.where("contact_email ~* ?", "cy.gov.uk").update_all(contact_email: nil)
-    Registration.where("applicant_email ~* ?", "cy.gov.uk").update_all(applicant_email: nil)
+    matching_registrations = find_email_variations(:applicant_email)
+
+    return unless matching_registrations.any?
+
+    Rails.logger.info "Clearing wex applicant email addresses: #{matching_registrations.pluck(:applicant_email)}"
+    matching_registrations.update_all(applicant_email: nil)
+
+    matching_registrations = find_email_variations(:contact_email)
+
+    return unless matching_registrations.any?
+
+    Rails.logger.info "Clearing wex contact email addresses: #{matching_registrations.pluck(:contact_email)}"
+    matching_registrations.update_all(contact_email: nil)
   end
+
+  def find_email_variations(email_type)
+    WasteExemptionsEngine::Registration.where("#{email_type} ~* ? or #{email_type} ~* ? or #{email_type} ~* ?", WASTE_EXEMPTIONS_EMAIL_VARIATIONS, EXEMPTION_EMAIL_VARIATIONS, NCCC_EMAIL_VARIATIONS)
+  end
+  # rubocop:enable Layout/LineLength
 end
