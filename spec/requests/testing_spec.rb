2a448a3bfb467add338e9c82bd372c11b8c53e6f
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-02-19 14:11:21 +0000

    Extend unit test coverage for testing controller

--- spec/requests/testing_spec.rb
+++ spec/requests/testing_spec.rb
@@ -11,8 +11,23 @@ RSpec.describe "Testing" do
   describe "/create_registration" do
     let(:expiry_date) { 2.days.from_now.strftime("%Y-%m-%d") }
 
-    it "creates a registration" do
-      expect { get "/testing/create_registration/#{expiry_date}" }.to change(WasteExemptionsEngine::Registration, :count).by(1)
+    context "when in a non-production environment" do
+      before { stub_const("ENV", ENV.to_hash.merge("AIRBRAKE_ENV_NAME" => "devwcr")) }
+
+      it "creates a registration" do
+        expect { get "/testing/create_registration/#{expiry_date}" }.to change(WasteExemptionsEngine::Registration, :count).by(1)
+      end
+    end
+
+    context "when in a production environment" do
+      before do
+        allow(Rails.env).to receive(:test?).and_return(false)
+        stub_const("ENV", ENV.to_hash.merge("AIRBRAKE_ENV_NAME" => "production"))
+      end
+
+      it "raises a routing error" do
+        expect { get "/testing/create_registration/#{expiry_date}" }.to raise_error ActionController::RoutingError
+      end
     end
   end
 end
