01135ed3d7ee9be3196df7deddb4f636e2db668c
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-06-14 18:47:02 +0100

    Feature/ruby 2514 edit expiry date (#1084)
    https://eaflood.atlassian.net/browse/RUBY-2514

---
+++ app/forms/modify_expiry_date_form.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class ModifyExpiryDateForm
+  include ActiveModel::Model
+
+  attr_reader :registration, :date_day, :date_month, :date_year
+
+  def initialize(registration)
+    @registration = registration
+    current_expiry_date = registration.registration_exemptions.first.expires_on
+    @date_day = current_expiry_date.day
+    @date_month = current_expiry_date.month
+    @date_year = current_expiry_date.year
+  end
+
+  def submit(params)
+    @date_day = params[:date_day].to_i
+    @date_month = params[:date_month].to_i
+    @date_year = params[:date_year].to_i
+
+    return false unless expiry_date
+
+    return false unless valid_future_date(expiry_date)
+
+    registration.registration_exemptions.each do |exemption|
+      exemption.update(expires_on: expiry_date)
+    end
+
+    true
+  rescue Date::Error, TypeError => e
+    Rails.logger.error "Error parsing registration date, params: #{params}: #{e}"
+    Airbrake.notify e, registration: registration.reference
+    false
+  end
+
+  private
+
+  def expiry_date
+    @expiry_date ||= Date.new(date_year, date_month, date_day)
+  rescue StandardError
+    errors.add :expiry_date, I18n.t(".modify_expiry_date.errors.invalid")
+    nil
+  end
+
+  def valid_future_date(date)
+    return true if date > Time.zone.today
+
+    errors.add :expiry_date, I18n.t(".modify_expiry_date.errors.not_future")
+    false
+  end
+end
