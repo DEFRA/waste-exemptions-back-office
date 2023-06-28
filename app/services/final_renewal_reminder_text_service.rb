3f0ffa8ca03600509f3b55309257755103a04d69
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-06-28 18:55:36 +0100

    Add communication logging to first_renewal_reminder_text_service

--- app/services/final_renewal_reminder_text_service.rb
+++ app/services/final_renewal_reminder_text_service.rb
@@ -3,6 +3,17 @@
 require "notifications/client"
 
 class FinalRenewalReminderTextService < RenewalReminderTextService
+
+  # For CanHaveCommunicationLog
+  def communications_log_params
+    {
+      message_type: "text",
+      template_id: template,
+      template_label: "First renewal reminder text",
+      sent_to: @registration.contact_phone
+    }
+  end
+
   private
 
   def template
--- app/services/renewal_reminder_text_service.rb
+++ app/services/renewal_reminder_text_service.rb
@@ -3,6 +3,8 @@
 require "notifications/client"
 
 class RenewalReminderTextService < RenewalReminderService
+  include WasteExemptionsEngine::CanHaveCommunicationLog
+
   def run(registration:)
     return unless registration.valid_mobile_phone_number?
 
@@ -10,8 +12,14 @@ class RenewalReminderTextService < RenewalReminderService
 
     client = Notifications::Client.new(WasteExemptionsEngine.configuration.notify_api_key)
 
-    client.send_sms(phone_number: @registration.contact_phone,
-                    template_id: template,
-                    personalisation: personalisation)
+    notify_result = client.send_sms(
+      phone_number: @registration.contact_phone,
+      template_id: template,
+      personalisation: personalisation
+    )
+
+    create_log(registration)
+
+    notify_result
   end
 end
