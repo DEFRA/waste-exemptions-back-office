2e1c70ddfaba6ecdd90affe6470527dad75c6b09
Leonid Brujev <brujev@gmail.com>
2023-08-22 16:18:04 +0100

    RUBY-2641: Communication History page

---
+++ app/controllers/communication_logs_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CommunicationLogsController < ApplicationController
+  def index
+    find_resource(params[:registration_reference])
+    authorize! :read, @resource
+
+    @communication_logs = @resource.communication_logs.order(created_at: :asc).page(params[:page]).per(10)
+  end
+
+  protected
+
+  def find_resource(reference)
+    @resource = WasteExemptionsEngine::Registration.find_by(reference: reference)
+  end
+end
