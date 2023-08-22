0c15a5925960045274902fa066134581594eb282
Leonid Brujev <brujev@gmail.com>
2023-08-22 17:24:03 +0100

    RUBY-2641: changing sorting to most recent

--- app/controllers/communication_logs_controller.rb
+++ app/controllers/communication_logs_controller.rb
@@ -5,7 +5,7 @@ class CommunicationLogsController < ApplicationController
     find_resource(params[:registration_reference])
     authorize! :read, @resource
 
-    @communication_logs = @resource.communication_logs.order(created_at: :asc).page(params[:page]).per(10)
+    @communication_logs = @resource.communication_logs.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   protected
