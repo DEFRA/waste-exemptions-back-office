6acbf8aa297cc2bf57c30fe63ccb8d68c333df12
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-09-20 19:47:09 +0100

    Replace resend deregistration email action with send edit invite email action

---
+++ spec/requests/send_edit_invite_email_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "sendEditInviteEmails" do
+  let(:notifications_client) { instance_double(Notifications::Client) }
+  let(:registration) { create(:registration) }
+  let(:referer) { "/" }
+
+  describe "GET /send-edit-invite-emails/:id" do
+    let(:request_path) { "/send-edit-invite-emails/#{registration.id}" }
+
+    before { sign_in(user) if defined?(user) }
+
+    context "when a valid user is signed in" do
+      let(:user) { create(:user, :admin_agent) }
+
+      context "when the service executes successfully" do
+        before do
+          allow(WasteExemptionsEngine::RegistrationEditLinkEmailService).to receive(:run).with(anything).and_call_original
+          allow(Notifications::Client).to receive(:new).and_return(notifications_client)
+          allow(notifications_client).to receive(:send_email).and_return(true)
+        end
+
+        # Bullet appears confused about eager-loading `registration_exemptions: :exemption`
+        # and `Bullet.unused_eager_loading_enable = false` does not disable it here, so turn it off and on again
+        around do |example|
+          Bullet.enable = false
+
+          example.run
+
+          Bullet.enable = true
+        end
+
+        it "calls the service and redirects back to the refering page with a success message" do
+          get request_path, params: {}, headers: { "HTTP_REFERER" => referer }
+
+          expect(WasteExemptionsEngine::RegistrationEditLinkEmailService)
+            .to have_received(:run).with(hash_including(registration: registration))
+
+          expect(response).to redirect_to(referer)
+
+          expect(flash[:message]).to eq("Edit invite email sent successfully")
+        end
+      end
+
+      context "when the service fails" do
+        before do
+          allow(WasteExemptionsEngine::RegistrationEditLinkEmailService).to receive(:run).and_return(false)
+        end
+
+        it "calls the service and redirects back to the refering page with a flash error message" do
+          get request_path, params: {}, headers: { "HTTP_REFERER" => referer }
+
+          expect(WasteExemptionsEngine::RegistrationEditLinkEmailService)
+            .to have_received(:run).with(hash_including(registration: registration))
+
+          expect(response).to redirect_to(referer)
+
+          expect(flash[:error]).to eq("Error sending edit invite email")
+        end
+      end
+    end
+
+    context "when a data agent user is signed in" do
+      let(:user) { create(:user, :data_agent) }
+
+      it "redirects to permission page" do
+        get request_path, params: {}, headers: { "HTTP_REFERER" => referer }
+        follow_redirect!
+
+        expect(response).to render_template("pages/permission")
+      end
+    end
+
+    context "when a valid user is not signed in" do
+      before { sign_out(create(:user)) }
+
+      it "redirects to the sign-in page" do
+        get request_path, params: {}, headers: { "HTTP_REFERER" => referer }
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
--- spec/support/shared_examples/abilities/developer_examples.rb
+++ spec/support/shared_examples/abilities/developer_examples.rb
@@ -8,8 +8,4 @@ RSpec.shared_examples "developer examples" do
   it "is able to view DEFRA quarterly reports" do
     expect(subject).to be_able_to(:read, Reports::DefraQuarterlyStatsService)
   end
-
-  it "is able to export deregistration email batches" do
-    expect(subject).to be_able_to(:manage, DeregistrationEmailExportsForm)
-  end
 end
