5772fd046b8b7c79b3c78c885399746a74775a2c
Leonid Brujev <brujev@gmail.com>
2024-04-29 16:05:12 +0100

    [RUBY-3050] helper method to display price in pence as pounds

---
+++ app/helpers/finance_details_helper.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module FinanceDetailsHelper
+  def display_pence_as_pounds_and_cents(pence, hide_pence_if_zero: false)
+    pounds = pence.to_f / 100
+
+    if (pounds % 1).zero?
+      hide_pence_if_zero ? format("%<pounds>.2f", pounds: pounds) : format("%<pounds>.0f", pounds: pounds)
+    else
+      format("%<pounds>.2f", pounds: pounds)
+    end
+  end
+end
