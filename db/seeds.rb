e6da90b398ab9fe202098a3429f0c10e479839b4
Leonid Brujev <brujev@gmail.com>
2024-04-29 16:05:59 +0100

    [RUBY-3050] seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,5 +18,29 @@ def seed_users
   end
 end
 
+def seed_charging_schemes
+  seeds = JSON.parse(Rails.root.join("db/seeds/charging_schemes.json").read)
+
+  # Bands
+  bands = seeds["bands"]
+  bands.each do |band|
+    WasteExemptionsEngine::Band.find_or_create_by(name: band["name"]) do |rec|
+      rec.sequence = band["sequence"]
+      rec.registration_fee = band["registration_fee"]
+      rec.initial_compliance_charge = band["initial_compliance_charge"]
+      rec.additional_compliance_charge = band["additional_compliance_charge"]
+    end
+  end
+
+  # Buckets
+  buckets = seeds["buckets"]
+  buckets.each do |bucket|
+    WasteExemptionsEngine::Bucket.find_or_create_by(name: bucket["name"]) do |rec|
+      rec.charge_amount = bucket["charge_amount"]
+    end
+  end
+end
+
 # Only seed if not running in production or we specifically require it, eg. for Heroku
 seed_users if !Rails.env.production? || ENV["ALLOW_SEED"]
+seed_charging_schemes if !Rails.env.production? || ENV["ALLOW_SEED"]
