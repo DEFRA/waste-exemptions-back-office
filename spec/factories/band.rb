0827c0eb1061a0a3456aa79dde3525fa28e485d5
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-05-14 15:38:22 +0100

    [RUBY-3066] Update band factory to generate unique sequence up to 100

--- spec/factories/band.rb
+++ spec/factories/band.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :band, class: "WasteExemptionsEngine::Band" do
     name { "Band #{Faker::Lorem.unique.word}" }
-    sequence(:sequence) { Faker::Number.unique.between(from: 1, to: 10) } # reserved word
+    sequence(:sequence) { Faker::Number.unique.between(from: 1, to: 100) } # reserved word
 
     initial_compliance_charge { association :charge, :initial_compliance_charge }
     additional_compliance_charge { association :charge, :additional_compliance_charge }
