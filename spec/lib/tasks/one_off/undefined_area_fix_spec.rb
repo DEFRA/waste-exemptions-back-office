75db6e0f40bf0e10aeb9b570c3d068d317592b88
Leonid Brujev <brujev@gmail.com>
2024-05-30 19:12:51 +0100

    [RUBY-3013] improving test coverage

--- spec/lib/tasks/one_off/undefined_area_fix_spec.rb
+++ spec/lib/tasks/one_off/undefined_area_fix_spec.rb
@@ -22,6 +22,12 @@ RSpec.describe "one_off:undefined_area_fix", type: :rake do
     reg2
   end
 
+  let(:reg_with_area_defined) do
+    reg1 = create(:registration, :site_uses_address, :with_manual_site_address, created_at: 1.year.ago)
+    reg1.site_address.update(postcode: "WA16 1TS", area: "Yorkshire")
+    reg1
+  end
+
   let(:coords) { { easting: 123.4, northing: 123.5 } }
 
   # By default Rails prevents multiple invocations of the same Rake task in succession
@@ -29,16 +35,20 @@ RSpec.describe "one_off:undefined_area_fix", type: :rake do
 
   it { expect { run_rake_task }.not_to raise_error }
 
-  it "updates the area for a registration with a site address postcode that can be found" do
+  it "defines the area for a registration with a site address postcode that can be found" do
     allow(WasteExemptionsEngine::DetermineEastingAndNorthingService).to receive(:run).with(grid_reference: nil, postcode: "WA16 0AW").and_return(coords)
     allow(WasteExemptionsEngine::DetermineAreaService).to receive(:run).and_return("Yorkshire")
     expect { run_rake_task }.to change { reg_with_postcode_that_can_be_found.site_address.reload.area }.to "Yorkshire"
   end
 
-  it "updates the area for a registration with a site address postcode that cannot be found" do
+  it "defines the area for a registration with a site address postcode that cannot be found" do
     allow(WasteExemptionsEngine::DetermineEastingAndNorthingService).to receive(:run).with(grid_reference: nil, postcode: "PE33 0NE").and_return(nil)
     allow(WasteExemptionsEngine::DetermineEastingAndNorthingService).to receive(:run).with(grid_reference: nil, postcode: "PE33 0N").and_return(coords)
     allow(WasteExemptionsEngine::DetermineAreaService).to receive(:run).and_return("Lincolnshire and Northamptonshire")
     expect { run_rake_task }.to change { reg_with_postcode_that_cannot_be_found.site_address.reload.area }.to "Lincolnshire and Northamptonshire"
+  end
+
+  it "doesn't change the area when area already defined" do
+    expect { run_rake_task }.not_to change { reg_with_area_defined.site_address.reload.area }
   end
 end
