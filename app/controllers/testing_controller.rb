d4fe3c07ee18f99f0d8c0a8d1767fbd80a9dae27
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-08-21 17:12:37 +0100

    [RUBY-3268] Add exemption handling and improve registration details display
    - Update `create_registration` to handle exemptions from params.
    - Add `registration_exemptions_by_codes` method for dynamic exemptions.
    - Rename `registration_exemptions` to `registration_exemptions_by_count`.
    - Enhance `show.html.erb` to display detailed registration and exemption information.

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -8,27 +8,43 @@ class TestingController < ApplicationController
 
   def create_registration
     @expiry_date = Date.parse(params[:expiry_date])
-
+    # exemptions from params[:exemptions] which is a list of exemption codes
+    # e.g. "create_registration/2022-01-01?exemptions=U1&exemptions=U2&exemptions=U3"
+    # "testing/create_registration/2022-01-01?exemptions[]=U4&exemptions[]=U5&exemptions[]=U1"
+    if params[:exemptions].present?
+      @registration_exemptions = registration_exemptions_by_codes(params[:exemptions])
+    else
+      @registration_exemptions = registration_exemptions_by_count(3)
+    end
     # https://github.com/thoughtbot/factory_bot/blob/ca810767e70ccd85c7cb63f775bc16f653a97dc8/GETTING_STARTED.md#rails-preloaders-and-rspec
     FactoryBot.reload
 
     registration = FactoryBot.create(:registration,
-                                     registration_exemptions: registration_exemptions(3))
+                                     registration_exemptions: registration_exemptions_by_count(3))
 
     # Ensure edit_token_created_at is populated
     registration.regenerate_and_timestamp_edit_token
 
-    render :show, locals: { registration: registration }
+    render :show, locals: { registration: registration, registration_exemptions: @registration_exemptions}
   end
 
   private
 
-  def registration_exemptions(count)
+  def registration_exemptions_by_count(count)
     selected_exemptions = WasteExemptionsEngine::Exemption.first(count)
     (0..count - 1).map do |n|
       FactoryBot.build(:registration_exemption,
                        expires_on: @expiry_date,
                        exemption: selected_exemptions[n] || FactoryBot.create(:exemption))
+    end
+  end
+
+  def registration_exemptions_by_codes(codes)
+    selected_exemptions = WasteExemptionsEngine::Exemption.where(code: codes)
+    codes.map do |code|
+      FactoryBot.build(:registration_exemption,
+                       expires_on: @expiry_date,
+                       exemption: selected_exemptions.find { |exemption| exemption.code == code } || FactoryBot.create(:exemption, code: code))
     end
   end
 
