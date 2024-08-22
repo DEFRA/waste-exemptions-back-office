74cef5a15be842d91583a0cbe160ae3d234eb424
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-08-22 11:56:55 +0100

    [RUBY-3268] rubocop fixes

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -11,11 +11,11 @@ class TestingController < ApplicationController
     # exemptions from params[:exemptions] which is a list of exemption codes
     # e.g. "create_registration/2022-01-01?exemptions=U1&exemptions=U2&exemptions=U3"
     # "testing/create_registration/2022-01-01?exemptions[]=U4&exemptions[]=U5&exemptions[]=U1"
-    registration_exemptions = if params[:exemptions].present?
-                                 registration_exemptions_by_codes(params[:exemptions])
-                               else
-                                 registration_exemptions_by_count(3)
-                               end
+    if params[:exemptions].present?
+      registration_exemptions_by_codes(params[:exemptions])
+    else
+      registration_exemptions_by_count(3)
+    end
     # https://github.com/thoughtbot/factory_bot/blob/ca810767e70ccd85c7cb63f775bc16f653a97dc8/GETTING_STARTED.md#rails-preloaders-and-rspec
     FactoryBot.reload
 
@@ -25,7 +25,7 @@ class TestingController < ApplicationController
     # Ensure edit_token_created_at is populated
     registration.regenerate_and_timestamp_edit_token
 
-    render :show, locals: { registration: registration, registration_exemptions: registration_exemptions }
+    render :show, locals: { registration: registration }
   end
 
   private
