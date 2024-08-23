d31a847576cfa8fa25053badd66192a0f22de55d
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-08-23 09:31:44 +0100

    Re-add missing devise schema attributes

--- db/schema.rb
+++ db/schema.rb
@@ -366,6 +366,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_24_150832) do
     t.datetime "updated_at", precision: nil, null: false
     t.string "role"
     t.boolean "active", default: true
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
