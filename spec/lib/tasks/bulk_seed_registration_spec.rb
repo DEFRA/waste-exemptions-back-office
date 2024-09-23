d6264adf102c2feab8660fc236bd0daaf83b7138
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-09-23 10:22:04 +0100

    Add unit test for search benchmark task

--- spec/lib/tasks/bulk_seed_registration_spec.rb
+++ spec/lib/tasks/bulk_seed_registration_spec.rb
@@ -6,9 +6,8 @@ RSpec.describe "Bulk seed registrations", type: :rake do
   include_context "rake"
 
   describe "bulk_seed_registration_exemptions" do
-
     after do
-      Rake::Task["cleanup:transient_registrations"].reenable
+      Rake::Task["bulk_seed_registration_exemptions"].reenable
     end
 
     it "runs without error" do
@@ -16,6 +15,26 @@ RSpec.describe "Bulk seed registrations", type: :rake do
         Rake::Task["bulk_seed_registration_exemptions"].invoke(3)
       end.not_to raise_error
     end
+  end
 
+  describe "benchmark_search" do
+
+    original_stdout = $stdout
+
+    before do
+      # suppress noisy outputs during unit test
+      $stdout = StringIO.new
+    end
+
+    after do
+      $stdout = original_stdout
+      Rake::Task["benchmark_search"].reenable
+    end
+
+    it "runs without error" do
+      expect do
+        Rake::Task["benchmark_search"].invoke("a")
+      end.not_to raise_error
+    end
   end
 end
