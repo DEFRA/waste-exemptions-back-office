4473a00299789bb2831f6fc3ee2e4427ce889daf
Leonid Brujev <brujev@gmail.com>
2024-11-06 00:03:52 +0000

    [RUBY-2705] AddPaymentForm and tests

--- app/forms/add_payment_form.rb
+++ app/forms/add_payment_form.rb
@@ -3,11 +3,23 @@
 class AddPaymentForm
   include ActiveModel::Model
 
-  attr_reader :registration, :payment_type, :payment_reference, :comments, :payment_amount, :date_day, :date_month,
+  attr_reader :account, :payment_type, :payment_reference, :comments, :payment_amount, :date_day, :date_month,
               :date_year
 
-  def initialize(registration)
-    @registration = registration
+  validates :payment_type,
+            inclusion: { in: %w[bank_transfer missing_card_payment other_payment],
+                         message: I18n.t(".payments.errors.payment_type_invalid") }
+  validates :payment_amount, presence: { message: I18n.t(".payments.errors.payment_amount_blank") },
+                             numericality: {
+                               greater_than: 0,
+                               message: I18n.t(".payments.errors.payment_amount_invalid")
+                             }
+  validates :payment_reference, presence: { message: I18n.t(".payments.errors.payment_reference_blank") }
+
+  validate :validate_payment_date_current_or_past
+
+  def initialize(account)
+    @account = account
   end
 
   def submit(params)
@@ -16,38 +28,39 @@ class AddPaymentForm
     @comments = params[:comments]
     @payment_amount = params[:payment_amount]
 
-    @date_day = params[:date_day]
-    @date_month = params[:date_month]
-    @date_year = params[:date_year]
+    @date_day = params[:date_day].to_i if params[:date_day].present?
+    @date_month = params[:date_month].to_i if params[:date_month].present?
+    @date_year = params[:date_year].to_i if params[:date_year].present?
 
-    return false unless payment_date
+    if valid?
+      payment = init_payment
+      return payment.save! if payment.valid?
+    end
 
-    return false unless valid_current_or_past_date(payment_date)
-
-    # registration.registration_exemptions.each do |exemption|
-    #   exemption.update(expires_on: expiry_date)
-    # end
-
-    true
-  rescue Date::Error, TypeError => e
-    Rails.logger.error "Error parsing registration date, params: #{params}: #{e}"
-    Airbrake.notify e, registration: registration.reference
     false
   end
 
   private
 
-  def payment_date
-    @expiry_date ||= Date.new(date_year, date_month, date_day)
-  rescue StandardError
-    errors.add :payment_date, I18n.t(".add_payment.errors.date_invalid")
-    nil
+  def init_payment
+    WasteExemptionsEngine::Payment.new(
+      payment_type: payment_type,
+      payment_amount: WasteExemptionsEngine::CurrencyConversionService.convert_pounds_to_pence(payment_amount),
+      payment_status: WasteExemptionsEngine::Payment::PAYMENT_STATUS_SUCCESS,
+      date_time: Date.new(date_year.to_i, date_month.to_i, date_day.to_i),
+      payment_uuid: SecureRandom.uuid,
+      reference: payment_reference,
+      account: account,
+      comments: comments
+    )
   end
 
-  def valid_current_or_past_date(date)
-    return true if date <= Time.zone.today
+  def validate_payment_date_current_or_past
+    date ||= Date.new(date_year, date_month, date_day)
+    return if date <= Time.zone.today
 
-    errors.add :payment_date, I18n.t(".add_payment.errors.date_not_current_or_past")
-    false
+    errors.add :payment_date, I18n.t(".payments.errors.date_not_current_or_past")
+  rescue StandardError
+    errors.add :payment_date, I18n.t(".payments.errors.date_invalid")
   end
 end
---
+++ spec/forms/add_payment_form_spec.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AddPaymentForm, type: :model do
+
+  subject(:form) { described_class.new(account) }
+
+  let(:registration) { create(:registration, :with_active_exemptions) }
+  let(:account) { create(:account, registration: registration) }
+
+  let(:date) { 1.day.ago.to_date }
+  let(:add_payment_form) do
+    {
+      payment_type: "bank_transfer",
+      payment_amount: 93.26,
+      payment_reference: "1234567890",
+      date_day: date.day.to_s,
+      date_month: date.month.to_s,
+      date_year: date.year.to_s,
+      comments: "test comments"
+    }
+  end
+
+  shared_examples "form submition fails" do |attr|
+    it "does not submit" do
+      expect(form.submit(add_payment_form)).to be(false)
+    end
+
+    it "contains an error" do
+      form.submit(add_payment_form)
+      expect(form.errors).to include(attr)
+    end
+
+    it "does not contain any other errors" do
+      form.submit(add_payment_form)
+      expect(form.errors.count).to eq(1)
+    end
+  end
+
+  describe "#submit" do
+    context "when the form is not valid" do
+      context "when the payment_type is invalid" do
+        before { add_payment_form[:payment_type] = "foo" }
+
+        it_behaves_like "form submition fails", :payment_type
+      end
+
+      context "when the date day is invalid" do
+        before { add_payment_form[:date_day] = 99 }
+
+        it_behaves_like "form submition fails", :payment_date
+      end
+
+      context "when the date month is invalid" do
+        before { add_payment_form[:date_month] = 99 }
+
+        it_behaves_like "form submition fails", :payment_date
+      end
+
+      context "when the date year is invalid" do
+        before { add_payment_form[:date_year] = 9999 }
+
+        it_behaves_like "form submition fails", :payment_date
+      end
+
+      context "when the payment_amount is invalid" do
+        before { add_payment_form[:payment_amount] = "abc" }
+
+        it_behaves_like "form submition fails", :payment_amount
+      end
+
+      context "when the payment_reference is nil" do
+        before { add_payment_form[:payment_reference] = nil }
+
+        it_behaves_like "form submition fails", :payment_reference
+      end
+    end
+
+    context "when the form is valid" do
+      it "submits" do
+        expect(form.submit(add_payment_form)).to be(true)
+      end
+
+      it "creates a new payment" do
+        expect { form.submit(add_payment_form) }.to change(WasteExemptionsEngine::Payment, :count).by(1)
+      end
+
+      it "creates a new payment with the correct attributes" do
+        form.submit(add_payment_form)
+
+        payment = WasteExemptionsEngine::Payment.last
+        expect(payment.payment_type).to eq("bank_transfer")
+        expect(payment.payment_amount).to eq(9326)
+        expect(payment.payment_status).to eq(WasteExemptionsEngine::Payment::PAYMENT_STATUS_SUCCESS)
+        expect(payment.date_time).to eq(date)
+        expect(payment.payment_uuid).not_to be_nil
+        expect(payment.reference).to eq("1234567890")
+        expect(payment.account).to eq(account)
+        expect(payment.comments).to eq("test comments")
+      end
+    end
+  end
+end
