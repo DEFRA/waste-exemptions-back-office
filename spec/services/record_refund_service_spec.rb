cfa88fe4475357a490dff490a9f43994ac6cb289
Jerome Pratt <jerome.pratt@hey.com>
2024-11-12 19:16:15 +0000

    [RUBY-2704] Add tests for `RecordRefundService` to handle refund processing
    This commit introduces a new test suite for the `RecordRefundService` to verify its functionality when processing refunds. It includes tests for successful refund creation, correct attribute assignment, account balance updates, and error handling with logging and Airbrake notification. The tests ensure that the service behaves as expected in both successful and error scenarios.

---
+++ spec/services/record_refund_service_spec.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+module WasteExemptionsEngine
+  RSpec.describe RecordRefundService do
+    subject(:service) { described_class.new }
+
+    let(:registration) { create(:registration) }
+    let(:account) { create(:account, registration: registration) }
+    let(:comments) { "Refund due to overpayment" }
+    let(:amount_in_pounds) { 100 }
+
+    describe "#run" do
+      context "when the refund is processed successfully" do
+        let(:payment) { create(:payment, account: account, payment_amount: 10_000) }
+
+        it "creates a new refund payment record" do
+          payment
+          expect { service.run(comments: comments, payment: payment, amount_in_pounds: amount_in_pounds) }
+            .to change(Payment, :count).by(1)
+        end
+
+        it "creates the refund with correct attributes" do
+          service.run(comments: comments, payment: payment, amount_in_pounds: amount_in_pounds)
+
+          refund = Payment.last
+          expect(refund).to have_attributes(
+            payment_type: Payment::PAYMENT_TYPE_REFUND,
+            payment_amount: -10_000,
+            payment_status: Payment::PAYMENT_STATUS_SUCCESS,
+            account_id: payment.account_id,
+            reference: "#{payment.reference}/REFUND",
+            comments: comments
+          )
+          expect(refund.payment_uuid).not_to be_nil
+        end
+
+        it "updates the account balance" do
+          allow(account).to receive(:update_balance)
+          allow(account).to receive(:save!)
+
+          service.run(comments: comments, payment: payment, amount_in_pounds: amount_in_pounds)
+          expect(account).to have_received(:update_balance)
+          expect(account).to have_received(:save!)
+        end
+
+        it "returns true" do
+          result = service.run(comments: comments, payment: payment, amount_in_pounds: amount_in_pounds)
+          expect(result).to be true
+        end
+      end
+
+      context "when an error occurs" do
+        let(:payment) { create(:payment, account: account, payment_amount: 10_000) }
+
+        before do
+          allow(Payment).to receive(:new).and_return(payment)
+          allow(payment).to receive(:save!).and_raise(StandardError)
+        end
+
+        it "logs the error" do
+          logger = instance_double(Logger)
+          allow(logger).to receive(:error)
+          allow(Rails).to receive(:logger).and_return(logger)
+          service.run(comments: comments, payment: payment, amount_in_pounds: amount_in_pounds)
+
+          expect(logger).to have_received(:error).with(/StandardError error processing refund for payment #{payment.id}/)
+        end
+
+        it "notifies Airbrake" do
+          allow(Airbrake).to receive(:notify)
+          service.run(comments: comments, payment: payment, amount_in_pounds: amount_in_pounds)
+
+          expect(Airbrake).to have_received(:notify).with(
+            instance_of(StandardError),
+            message: "Error processing refund for payment ",
+            payment_id: payment.id
+          )
+        end
+
+        it "returns false" do
+          result = service.run(comments: comments, payment: payment, amount_in_pounds: amount_in_pounds)
+          expect(result).to be false
+        end
+
+        it "does not create a new payment record" do
+          expect do
+            service.run(comments: comments, payment: payment, amount_in_pounds: amount_in_pounds)
+          end.not_to change(Payment, :count)
+        end
+      end
+    end
+  end
+end
