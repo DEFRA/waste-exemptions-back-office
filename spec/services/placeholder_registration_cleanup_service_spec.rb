33db4892b6478f0f20150c84bc080de1fe55e666
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2024-11-18 16:12:17 +0000

    Fix placeholder cleanup service specs

--- spec/services/placeholder_registration_cleanup_service_spec.rb
+++ spec/services/placeholder_registration_cleanup_service_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe PlaceholderRegistrationCleanupService do
 
     subject(:run_service) { described_class.run }
 
-    let!(:placeholder_registration_5_days) { create(:registration, placeholder: true, created_at: 5.days.ago) }
-    let!(:placeholder_registration_8_days) { create(:registration, placeholder: true, created_at: 8.days.ago) }
-    let!(:completed_registration) { create(:registration, placeholder: true) }
-    let!(:nil_status_registration) { create(:registration, placeholder: false) }
+    let!(:placeholder_registration_5_days_id) { create(:registration, placeholder: true, created_at: 5.days.ago).id }
+    let!(:placeholder_registration_8_days_id) { create(:registration, placeholder: true, created_at: 8.days.ago).id }
+    let!(:non_placeholder_registration_id) { create(:registration, placeholder: false, created_at: 8.days.ago).id }
+    let!(:nil_placeholder_registration_id) { create(:registration, placeholder: nil, created_at: 8.days.ago).id }
 
     before { allow(Rails.configuration).to receive(:max_transient_registration_age_days).and_return(7) }
 
     it { expect { run_service }.to change(WasteExemptionsEngine::Registration, :count).by(-1) }
 
-    it { expect { run_service }.to change { WasteExemptionsEngine::Registration.find(placeholder_registration_5_days.id) }.to(nil) }
+    it { expect { run_service }.to change { WasteExemptionsEngine::Registration.find_by(id: placeholder_registration_8_days_id) }.to(nil) }
 
-    it { expect { run_service }.not_to change { WasteExemptionsEngine::Registration.find(placeholder_registration_8_days.id) } }
+    it { expect { run_service }.not_to change { WasteExemptionsEngine::Registration.find_by(id: placeholder_registration_5_days_id) } }
 
-    it { expect { run_service }.not_to change { WasteExemptionsEngine::Registration.find(completed_registration.id) } }
+    it { expect { run_service }.not_to change { WasteExemptionsEngine::Registration.find_by(id: non_placeholder_registration_id) } }
 
-    it { expect { run_service }.not_to change { WasteExemptionsEngine::Registration.find(nil_status_registration.id) } }
+    it { expect { run_service }.not_to change { WasteExemptionsEngine::Registration.find_by(id: nil_placeholder_registration_id) } }
   end
 end
