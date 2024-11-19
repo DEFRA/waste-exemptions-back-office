6fbce16b785daa16f4523bb1e80e53eb8e847956
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-11-19 10:32:52 +0000

    Resolve merge conflicts

---
+++ app/presenters/payment_details_presenter.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class PaymentDetailsPresenter
+  include ActionView::Helpers::NumberHelper
+  include FinanceDetailsHelper
+
+  attr_reader :registration
+
+  delegate :account, to: :registration
+
+  def initialize(registration)
+    @registration = registration
+  end
+
+  def orders
+    @orders ||= account&.orders&.sort_by(&:created_at)&.reverse || []
+  end
+
+  def payments
+    @payments ||= account
+                  &.payments
+                  &.where&.not(payment_type: WasteExemptionsEngine::Payment::PAYMENT_TYPE_REFUND)
+                  &.order(date_time: :desc)
+  end
+
+  def refunds
+    @refunds ||= account
+                 &.payments
+                 &.where(payment_type: WasteExemptionsEngine::Payment::PAYMENT_TYPE_REFUND)
+                 &.order(date_time: :desc)
+  end
+
+  def balance
+    return nil unless registration.account&.balance
+
+    display_pence_as_pounds_and_cents(registration.account.balance)
+  end
+
+  def can_display_refund_link?
+    registration.account&.overpaid?
+  end
+
+  def format_date(datetime)
+    datetime.to_date.to_fs(:day_month_year_slashes)
+  end
+
+  def order_exemption_codes(order)
+    order.exemptions.map(&:code).sort.join(",")
+  end
+end
