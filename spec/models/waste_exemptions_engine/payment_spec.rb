ca63a62897410cc23e794f0f03c2257f4c33bb63
Jerome Pratt <jerome.pratt@hey.com>
2024-12-09 12:50:09 +0000

    [RUBY-2704] Fix scoping issues in `Payment` model and update tests
    - Extend `Payment` class from `ApplicationRecord` for ActiveRecord functionality.
    - Update `refunds_and_reversals` and `refundable` scopes to include `.success` for filtering successful payments.
    - Add comprehensive tests for all payment scopes, ensuring correct filtering and ordering of payments.
    - Refactor test setup for payment types and status to improve clarity and maintainability.

--- spec/models/waste_exemptions_engine/payment_spec.rb
+++ spec/models/waste_exemptions_engine/payment_spec.rb
@@ -1,28 +1,29 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require WasteExemptionsEngine::Engine.root.join("app", "models", "waste_exemptions_engine", "payment")
 
 module WasteExemptionsEngine
   RSpec.describe Payment do
-    let(:registration) { create(:registration) }
-    let(:account) { create(:account, registration:) }
-
     describe "#maximum_refund_amount" do
+      let(:payment_amount) { 100 }
+      let(:payment) { create(:payment, payment_amount:) }
+
       context "when payment type is not refundable" do
-        let(:reversal_payment) { create(:payment, payment_type: Payment::PAYMENT_TYPE_REVERSAL, account:) }
+        before do
+          allow(payment).to receive(:payment_type).and_return(Payment::PAYMENT_TYPE_REVERSAL)
+        end
 
         it "returns nil" do
-          expect(reversal_payment.maximum_refund_amount).to be_nil
+          expect(payment.maximum_refund_amount).to be_nil
         end
       end
 
       context "when payment type is refundable" do
-        let(:payment_amount) { 100 }
-        let(:refundable_payment) do
-          create(:payment,
-                 payment_type: Payment::PAYMENT_TYPE_BANK_TRANSFER,
-                 payment_amount: payment_amount,
-                 account:)
+        let(:account) { instance_double(Account) }
+
+        before do
+          allow(payment).to receive_messages(payment_type: Payment::PAYMENT_TYPE_BANK_TRANSFER, account: account)
         end
 
         context "when payment amount is less than account balance" do
@@ -31,7 +32,7 @@ module WasteExemptionsEngine
           end
 
           it "returns the payment amount" do
-            expect(refundable_payment.maximum_refund_amount).to eq(payment_amount)
+            expect(payment.maximum_refund_amount).to eq(payment_amount)
           end
         end
 
@@ -41,43 +42,90 @@ module WasteExemptionsEngine
           end
 
           it "returns the account balance" do
-            expect(refundable_payment.maximum_refund_amount).to eq(50)
+            expect(payment.maximum_refund_amount).to eq(50)
           end
         end
       end
     end
 
-    describe "#successful_payments" do
-      let(:successful_payments_scope) { instance_double(ActiveRecord::Relation) }
+    describe "scopes" do
+      describe ".not_cancelled" do
+        it "excludes cancelled payments" do
+          cancelled_payment = create(:payment, payment_status: Payment::PAYMENT_STATUS_CANCELLED)
+          success_payment = create(:payment, payment_status: Payment::PAYMENT_STATUS_SUCCESS)
+          started_payment = create(:payment, payment_status: Payment::PAYMENT_STATUS_STARTED)
 
-      it "calls successful_payments on the payments association" do
-        allow(account.payments).to receive(:successful_payments).and_return(successful_payments_scope)
-        account.successful_payments
+          result = described_class.not_cancelled
 
-        expect(account.payments).to have_received(:successful_payments)
+          expect(result).to include(success_payment, started_payment)
+          expect(result).not_to include(cancelled_payment)
+        end
       end
 
-      it "returns the successful_payments scope" do
-        allow(account.payments).to receive(:successful_payments).and_return(successful_payments_scope)
+      describe ".refunds_and_reversals" do
+        let!(:refund) { create(:payment, payment_type: Payment::PAYMENT_TYPE_REFUND) }
+        let!(:reversal) { create(:payment, payment_type: Payment::PAYMENT_TYPE_REVERSAL) }
+        let!(:regular_payment) { create(:payment, payment_type: Payment::PAYMENT_TYPE_BANK_TRANSFER) }
 
-        expect(account.successful_payments).to eq(successful_payments_scope)
+        before do
+          allow(refund).to receive(:success?).and_return(true)
+          allow(reversal).to receive(:success?).and_return(true)
+        end
+
+        it "returns only refunds and reversals ordered by date" do
+          result = described_class.refunds_and_reversals
+
+          expect(result).to include(refund, reversal)
+          expect(result).not_to include(regular_payment)
+        end
       end
-    end
 
-    describe "#refunds_and_reversals" do
-      let(:refunds_scope) { instance_double(ActiveRecord::Relation) }
+      describe ".excluding_refunds_and_reversals" do
+        let!(:refund) { create(:payment, payment_type: Payment::PAYMENT_TYPE_REFUND) }
+        let!(:reversal) { create(:payment, payment_type: Payment::PAYMENT_TYPE_REVERSAL) }
+        let!(:regular_payment) { create(:payment, payment_type: Payment::PAYMENT_TYPE_BANK_TRANSFER) }
 
-      it "calls refunds_and_reversals on the payments association" do
-        allow(account.payments).to receive(:refunds_and_reversals).and_return(refunds_scope)
-        account.refunds_and_reversals
+        it "excludes refunds and reversals" do
+          result = described_class.excluding_refunds_and_reversals
 
-        expect(account.payments).to have_received(:refunds_and_reversals)
+          expect(result).to include(regular_payment)
+          expect(result).not_to include(refund, reversal)
+        end
       end
 
-      it "returns the refunds_and_reversals scope" do
-        allow(account.payments).to receive(:refunds_and_reversals).and_return(refunds_scope)
+      describe ".refundable" do
+        let!(:bank_transfer) { create(:payment, payment_type: Payment::PAYMENT_TYPE_BANK_TRANSFER) }
+        let!(:govpay) { create(:payment, payment_type: Payment::PAYMENT_TYPE_GOVPAY) }
+        let!(:refund) { create(:payment, payment_type: Payment::PAYMENT_TYPE_REFUND) }
 
-        expect(account.refunds_and_reversals).to eq(refunds_scope)
+        before do
+          allow(bank_transfer).to receive(:success?).and_return(true)
+          allow(govpay).to receive(:success?).and_return(true)
+          allow(refund).to receive(:success?).and_return(true)
+        end
+
+        it "returns only successful payments with refundable payment types" do
+          result = described_class.refundable
+
+          expect(result).to include(bank_transfer, govpay)
+          expect(result).not_to include(refund)
+        end
+      end
+
+      describe ".successful_payments" do
+        let!(:successful_payment) { create(:payment, payment_type: Payment::PAYMENT_TYPE_BANK_TRANSFER, payment_status: Payment::PAYMENT_STATUS_SUCCESS) }
+        let!(:refund) { create(:payment, payment_type: Payment::PAYMENT_TYPE_REFUND, payment_status: Payment::PAYMENT_STATUS_SUCCESS) }
+
+        before do
+          allow(successful_payment).to receive(:success?).and_return(true)
+        end
+
+        it "returns successful payments excluding refunds and reversals" do
+          result = described_class.successful_payments
+
+          expect(result).to include(successful_payment)
+          expect(result).not_to include(refund)
+        end
       end
     end
   end
