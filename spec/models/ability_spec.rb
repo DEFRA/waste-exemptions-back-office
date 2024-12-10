48e93a216d8837d11e40256ac15c64212c3dcb33
Leonid Brujev <brujev@gmail.com>
2024-12-10 11:11:02 +0000

    [RUBY-3384] Adding read_quarterly_stats permission for service_manager role

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -71,7 +71,7 @@ RSpec.describe Ability do
     it_behaves_like "can manage users"
     it_behaves_like "can manage registrations"
 
-    it { expect(ability).not_to be_able_to(:read, Reports::DefraQuarterlyStatsService) }
+    it { expect(ability).to be_able_to(:read, Reports::DefraQuarterlyStatsService) }
     it { expect(ability).to be_able_to(:read, Reports::Download) }
   end
 
