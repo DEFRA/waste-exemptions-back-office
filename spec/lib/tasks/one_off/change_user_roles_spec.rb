a1afc5854460c4fe71fab87e447b0a38f4c98cb7
Leonid Brujev <brujev@gmail.com>
2024-12-16 16:08:14 +0000

    [RUBY-3388] adding one_off rake task to change user roles from "data_agent" to "data_viewer"

---
+++ spec/lib/tasks/one_off/change_user_roles_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "one_off:change_user_roles", type: :rake do
+
+  subject(:run_rake_task) { rake_task.invoke }
+
+  include_context "rake"
+
+  let(:rake_task) { Rake::Task["one_off:change_user_roles"] }
+  let(:user) { create(:user) }
+
+  # By default Rails prevents multiple invocations of the same Rake task in succession
+  after { rake_task.reenable }
+
+  it { expect { run_rake_task }.not_to raise_error }
+
+  context "when the user role is data_agent" do
+    it "changes the role name to data_viewer" do
+      user.role = "data_agent"
+      user.save(validate: false)
+
+      expect { run_rake_task }.to change { user.reload.role }.to("data_viewer")
+    end
+  end
+end
