88ae53458ff97fea27a42ea0ce839a303dfac4fe
Jerome Pratt <jerome.pratt@hey.com>
2024-12-30 15:26:54 +0000

    [RUBY-3406] Refactor `run_until_done` method to use `run` method
    Updated the `CompaniesHouseNameMatchingRunnerService` to call `CompaniesHouseNameMatchingBatchService.run` instead of `run_batch` for improved clarity and consistency in method naming.

--- app/services/companies_house_name_matching_runner_service.rb
+++ app/services/companies_house_name_matching_runner_service.rb
@@ -3,7 +3,7 @@
 class CompaniesHouseNameMatchingRunnerService < WasteExemptionsEngine::BaseService
   def self.run_until_done(dry_run: true, report_path: nil)
     loop do
-      result = CompaniesHouseNameMatchingBatchService.run_batch(dry_run: dry_run, report_path: report_path)
+      result = CompaniesHouseNameMatchingBatchService.run(dry_run: dry_run, report_path: report_path)
 
       # If we didn't process anything or there's nothing left, we're done.
       break unless result[:any_left_to_process?]
