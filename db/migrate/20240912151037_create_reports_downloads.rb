8b2d14826aee1de222dabbf48818262214621847
Jerome Pratt <jerome.pratt@hey.com>
2025-01-10 08:39:26 +0000

    [RUBY-3406] Update branch name and migration file
    - Changed the branch name in `Gemfile` and `Gemfile.lock` to `RUBY-3406-wex-company-name-rechecks-via-companies-house-re-checking-data-cleanse-3`.
    - Updated the `revision` in `Gemfile.lock`.
    - Removed the conditional check for table existence in the migration file `20240912151037_create_reports_downloads.rb` to always create the `reports_downloads` table.

--- db/migrate/20240912151037_create_reports_downloads.rb
+++ db/migrate/20240912151037_create_reports_downloads.rb
@@ -1,13 +1,11 @@
 class CreateReportsDownloads < ActiveRecord::Migration[7.1]
   def change
-    unless table_exists?(:reports_downloads)
-      create_table :reports_downloads do |t|
-        t.string :report_type
-        t.string :report_file_name
+    create_table :reports_downloads do |t|
+      t.string :report_type
+      t.string :report_file_name
 
-        t.string :user_id
-        t.datetime :downloaded_at
-      end
+      t.string :user_id
+      t.datetime :downloaded_at
     end
   end
 end
