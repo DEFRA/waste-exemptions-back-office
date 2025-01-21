e77b3323d96d3af1d4567f6e1a28d0633617a07d
Jerome Pratt <jerome.pratt@hey.com>
2025-01-21 19:38:42 +0000

    [RUBY-3571] Remove beta start view and add factories and request specs for beta feature
    - Deleted `beta_start/show.html.erb` view file as it's no longer needed.
    - Added `beta_participant` and `new_charged_registration` factories to support testing.
    - Introduced `beta_start_spec.rb` request specs to test the beta start feature, including scenarios for feature activation and user access control.

---
+++ spec/requests/beta_start_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Beta Start", type: :request do
+  let(:registration) { create(:registration) }
+
+  describe "GET /registrations/:reference/beta_start" do
+    let(:request_path) { "/registrations/#{registration.reference}/beta_start" }
+
+    before do
+      sign_in(user) if defined?(user)
+      allow(WasteExemptionsEngine::FeatureToggle).to receive(:active?).with(:private_beta).and_return(feature_active)
+    end
+
+    context "when private beta feature is active" do
+      let(:feature_active) { true }
+
+      shared_examples "allows access to beta start" do
+        it "creates a new beta participant and redirects to the beta start form" do
+          get request_path
+
+          expect(WasteExemptionsEngine::BetaParticipant.count).to eq(1)
+          participant = WasteExemptionsEngine::BetaParticipant.last
+
+          expect(participant.email).to eq(registration.contact_email)
+
+          path = WasteExemptionsEngine::Engine.routes.url_helpers.new_beta_start_form_path(participant.token)
+          expect(response).to redirect_to(path)
+        end
+      end
+
+      shared_examples "denies access to beta start" do
+        it "redirects to permission page" do
+          get request_path
+          follow_redirect!
+
+          expect(response).to redirect_to("/pages/permission")
+        end
+      end
+
+      context "when user is an admin team user" do
+        let(:user) { create(:user, :admin_team_user) }
+        include_examples "allows access to beta start"
+      end
+
+      context "when user is not signed in" do
+        before { sign_out(create(:user)) }
+
+        it "redirects to the sign-in page" do
+          get request_path
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+    end
+  end
+end
