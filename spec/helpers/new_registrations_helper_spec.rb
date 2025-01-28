62bdda79dbea4d3938b95f42a023d4ea385ef075
Leonid Brujev <brujev@gmail.com>
2025-01-28 12:45:59 +0000

    [RUBY-3554] Adding a link to private beta participants page

---
+++ spec/helpers/new_registrations_helper_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe RegistrationsHelper do
+  let(:resource) { build(:new_registration) }
+
+  describe "#back_path" do
+    context "when the back_to param is present" do
+      before { allow(helper).to receive(:params).and_return(back_to: "/foo") }
+
+      it "returns the back_to param" do
+        expect(helper.back_path).to eq "/foo"
+      end
+    end
+
+    context "when the back_to param is not present" do
+      before { allow(helper).to receive(:params).and_return({}) }
+
+      it "returns the root path" do
+        expect(helper.back_path).to eq root_path
+      end
+    end
+  end
+end
--- spec/helpers/registrations_helper_spec.rb
+++ spec/helpers/registrations_helper_spec.rb
@@ -127,4 +127,22 @@ RSpec.describe RegistrationsHelper do
       it_behaves_like "is / is not a private beta participant"
     end
   end
+
+  describe "#back_path" do
+    context "when the back_to param is present" do
+      before { allow(helper).to receive(:params).and_return(back_to: "/foo") }
+
+      it "returns the back_to param" do
+        expect(helper.back_path).to eq "/foo"
+      end
+    end
+
+    context "when the back_to param is not present" do
+      before { allow(helper).to receive(:params).and_return({}) }
+
+      it "returns the root path" do
+        expect(helper.back_path).to eq root_path
+      end
+    end
+  end
 end
