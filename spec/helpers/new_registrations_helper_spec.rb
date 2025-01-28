f3838af424ce9cafd36bbf7fa2eb253e4935ca28
Leonid Brujev <brujev@gmail.com>
2025-01-28 16:16:52 +0000

    [RUBY-3554] Refactor RegistrationsHelper and NewRegistrationsHelper

--- spec/helpers/new_registrations_helper_spec.rb
+++ spec/helpers/new_registrations_helper_spec.rb
@@ -7,16 +7,16 @@ RSpec.describe NewRegistrationsHelper do
 
   describe "#back_path" do
     context "when the back_to param is present" do
-      before { allow(helper).to receive(:params).and_return(back_to: "/foo") }
-
+      before do
+        allow(helper).to receive(:params).and_return({ back_to: "/some-path" })
+      end
+  
       it "returns the back_to param" do
-        expect(helper.back_path).to eq "/foo"
+        expect(helper.back_path).to eq "/some-path"
       end
     end
 
     context "when the back_to param is not present" do
-      before { allow(helper).to receive(:params).and_return({}) }
-
       it "returns the root path" do
         expect(helper.back_path).to eq root_path
       end
