e0a73e72ecdd904cba23f0639b72f0cb2e0a138e
Leonid Brujev <brujev@gmail.com>
2025-01-28 17:24:25 +0000

    [RUBY-3554] Refactor NewRegistrationsHelperSpec

--- spec/helpers/new_registrations_helper_spec.rb
+++ spec/helpers/new_registrations_helper_spec.rb
@@ -2,16 +2,25 @@
 
 require "rails_helper"
 
+class DummyClass
+  include NewRegistrationsHelper
+
+  attr_accessor :params, :root_path
+end
+
 RSpec.describe NewRegistrationsHelper do
   let(:resource) { build(:new_registration) }
+  let(:helper) do
+    helper = DummyClass.new 
+    helper.params = {}
+    helper.root_path = root_path
+    helper
+  end
 
   describe "#back_path" do
     context "when the back_to param is present" do
-      before do
-        allow(helper).to receive(:params).and_return({ back_to: "/some-path" })
-      end
-
       it "returns the back_to param" do
+        helper.params = { back_to: "/some-path" }
         expect(helper.back_path).to eq "/some-path"
       end
     end
