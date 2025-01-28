86a038ee08ca75c855a99379120389d43ffc55bc
Leonid Brujev <brujev@gmail.com>
2025-01-28 12:56:50 +0000

    [RUBY-3554] Refactor beta participant factory

--- spec/factories/beta_participant.rb
+++ spec/factories/beta_participant.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     reg_number { "MyString" }
     email { "MyString" }
     token { "MyString" }
-    invited_at { 1.day.ago }
+    invited_at { nil }
     opted_in { nil }
     registration { nil }
   end
