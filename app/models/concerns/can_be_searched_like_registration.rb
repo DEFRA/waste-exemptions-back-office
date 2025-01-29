076ab865000cfe17fac7a4abf9a0ece4da77c58a
Paul Doyle <97455399+PaulDoyle-EA@users.noreply.github.com>
2025-01-29 13:11:26 +0000

    Include new_charged_registrations in "New registrations" search results (#1608)
    https://eaflood.atlassian.net/browse/RUBY-3579

--- app/models/concerns/can_be_searched_like_registration.rb
+++ app/models/concerns/can_be_searched_like_registration.rb
@@ -4,7 +4,8 @@ module CanBeSearchedLikeRegistration
   extend ActiveSupport::Concern
 
   included do
-    scope :search_registration_and_relations, lambda { |term|
+    # Allow classes using the concern to extend scope logic
+    scope :base_search_registration_and_relations, lambda { |term|
       where(id: search_registration(term).ids +
                 search_for_site_address_postcode(term).ids +
                 search_for_contact_address_postcode(term).ids +
@@ -12,6 +13,10 @@ module CanBeSearchedLikeRegistration
                 search_for_person_name(term).ids +
                 search_for_telephone(term).ids)
         .order(created_at: :desc)
+    }
+
+    scope :search_registration_and_relations, lambda { |term|
+      base_search_registration_and_relations(term)
     }
 
     scope :search_registration, lambda { |term|
