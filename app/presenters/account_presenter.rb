40a736e68ab5aa302019ae921a81d734e9466416
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-01-30 14:29:01 +0000

    Add account.nil guards and unit tests to account_presenter

--- app/presenters/account_presenter.rb
+++ app/presenters/account_presenter.rb
@@ -4,24 +4,32 @@ class AccountPresenter < BasePresenter
   include FinanceDetailsHelper
 
   def balance
-    return nil unless super
+    return nil if __getobj__.blank?
 
     display_pence_as_pounds_sterling_and_pence(pence: super)
   end
 
   def successful_payments
+    return [] if __getobj__.blank?
+
     super.map { |payment| PaymentPresenter.new(payment) }
   end
 
   def refunds_and_reversals
+    return [] if __getobj__.blank?
+
     super.map { |payment| PaymentPresenter.new(payment) }
   end
 
   def sorted_orders
+    return [] if __getobj__.blank?
+
     orders.order(created_at: :desc).map { |order| OrderPresenter.new(order) }
   end
 
   def charge_adjustments
+    return [] if __getobj__.blank?
+
     super.order(created_at: :desc).map { |charge_adjustment| ChargeAdjustmentPresenter.new(charge_adjustment) }
   end
 end
