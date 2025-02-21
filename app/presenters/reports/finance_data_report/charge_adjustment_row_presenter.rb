d29060b233ead4a0bd03b74c7bc63822d4f6a577
Leonid Brujev <brujev@gmail.com>
2025-02-21 12:00:59 +0000

    [RUBY-3658] refactoring ChargeAdjustmentRowPresenter to show a negative amount when the adjustment type is "decrease"

--- app/presenters/reports/finance_data_report/charge_adjustment_row_presenter.rb
+++ app/presenters/reports/finance_data_report/charge_adjustment_row_presenter.rb
@@ -8,13 +8,19 @@ module Reports
       end
 
       def charge_amount
-        display_pence_as_pounds_and_pence(pence: @secondary_object.amount, hide_pence_if_zero: true)
+        display_pence_as_pounds_and_pence(pence: charge_adjustment_amount, hide_pence_if_zero: true)
       end
 
       def balance
-        @total += @secondary_object.amount
+        @total += charge_adjustment_amount
         display_pence_as_pounds_and_pence(pence: @total,
                                           hide_pence_if_zero: true)
+      end
+
+      private
+
+      def charge_adjustment_amount
+        @secondary_object.adjustment_type == "decrease" ? (0 - @secondary_object.amount) : @secondary_object.amount
       end
     end
   end
