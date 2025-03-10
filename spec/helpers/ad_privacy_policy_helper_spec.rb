27156536177e13c0ffe3f6cd88111ca2d28b3a10
Leonid Brujev <brujev@gmail.com>
2025-03-10 15:32:49 +0000

    [RUBY-3690] make new registration workflow to start from location page

--- spec/helpers/ad_privacy_policy_helper_spec.rb
+++ spec/helpers/ad_privacy_policy_helper_spec.rb
@@ -18,34 +18,21 @@ RSpec.describe AdPrivacyPolicyHelper do
     context "when no registration is present" do
       let(:registration) { nil }
 
-      context "when the ad_charged_journey_link feature toggle is not enabled" do
-        before do
-          allow(WasteExemptionsEngine::FeatureToggle).to receive(:active?)
-            .with(:ad_charged_journey_link).and_return(false)
-        end
-
-        it "returns the non-charged new registration start path" do
-          expect(destination_path).to eq WasteExemptionsEngine::Engine.routes.url_helpers.new_start_form_path
-        end
+      before do
+        allow(WasteExemptionsEngine::FeatureToggle).to receive(:active?)
+          .with(:ad_charged_journey_link).and_return(true)
       end
 
-      context "when the ad_charged_journey_link feature toggle is enabled" do
-        before do
-          allow(WasteExemptionsEngine::FeatureToggle).to receive(:active?)
-            .with(:ad_charged_journey_link).and_return(true)
-        end
+      it "creates a new charged registration" do
+        expect { helper.destination_path }.to change(WasteExemptionsEngine::NewChargedRegistration, :count).by(1)
+      end
 
-        it "creates a new charged registration" do
-          expect { helper.destination_path }.to change(WasteExemptionsEngine::NewChargedRegistration, :count).by(1)
-        end
-
-        it "returns the path to the location page" do
-          path = destination_path
-          transient_registration = WasteExemptionsEngine::NewChargedRegistration.last
-          expect(path).to eq WasteExemptionsEngine::Engine
-            .routes.url_helpers
-            .new_location_form_path(transient_registration.token)
-        end
+      it "returns the path to the location page" do
+        path = destination_path
+        transient_registration = WasteExemptionsEngine::NewChargedRegistration.last
+        expect(path).to eq WasteExemptionsEngine::Engine
+          .routes.url_helpers
+          .new_location_form_path(transient_registration.token)
       end
     end
   end
