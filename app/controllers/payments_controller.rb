d35b58c99c2b5de1b19e67f5b0e4838f9ad898fb
brujeo <brujev@gmail.com>
2025-03-11 15:23:49 +0000

    [RUBY-3674] Adding background job to check registration balance and send confirmation email when balance is fully paid (#1704)
    Co-authored-by: Jerome Pratt <pratt.jerome@environment-agency.gov.uk>

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,6 +9,8 @@ class PaymentsController < ApplicationController
 
   def create
     if add_payment_form.submit(payment_params)
+      SendRegistrationConfirmationWhenBalanceFullyPaidJob.perform_later(reference: resource.reference)
+
       successful_redirection = WasteExemptionsEngine::ApplicationController::SUCCESSFUL_REDIRECTION_CODE
       redirect_to registration_payment_details_path(reference: resource.reference), status: successful_redirection
     else
