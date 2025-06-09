17925cec4f8084b6bfb86915c782aa0daf803345
Leonid Brujev <brujev@gmail.com>
2025-06-09 23:21:54 +0100

    Refactor email_spec.rb to improve output expectations and remove unnecessary comments

--- spec/lib/tasks/email_spec.rb
+++ spec/lib/tasks/email_spec.rb
@@ -6,17 +6,9 @@ require "rails_helper"
 RSpec.describe "Email task", type: :rake do
   include_context "rake"
 
-  # We have this test declared here as a way of recording a decision. The
-  # test requires that the env var EMAIL_TEST_ADDRESS is specified, which
-  # is no big problem but is a slight hindrance. Mainly it's because when run
-  # it just spits out text to the console. This messes up our rspec output
-  # format, and we don't like that!
-  #
-  # If this was a more important function we'd properly cover it, but in this
-  # case we are happy to skip the test coverage.
-  describe "email:test", skip: "We don't want to cover this!" do
+  describe "email:test" do
     it "runs without error" do
-      expect { subject.invoke }.not_to raise_error
+      expect { subject.invoke }.to output(/From: Waste Exemptions Service/).to_stdout
     end
   end
 
