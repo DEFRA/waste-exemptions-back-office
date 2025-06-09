99402876109a9f097825de5385e0c166e72a7966
Leonid Brujev <brujev@gmail.com>
2025-06-09 22:32:06 +0100

    Refactor govpay_spec.rb to improve output expectations and remove unnecessary output suppression

--- spec/lib/tasks/test/govpay_spec.rb
+++ spec/lib/tasks/test/govpay_spec.rb
@@ -5,20 +5,6 @@ require "rails_helper"
 RSpec.describe "govpay test tasks", type: :rake do
   include_context "rake"
 
-  original_stdout = $stdout
-  original_stderr = $stderr
-
-  before do
-    # suppress noisy outputs during unit test
-    $stdout = StringIO.new
-    $stderr = StringIO.new
-  end
-
-  after do
-    $stdout = original_stdout
-    $stderr = original_stderr
-  end
-
   describe "test:govpay:signature" do
 
     subject(:run_rake_task) { rake_task.invoke("test_body") }
@@ -29,7 +15,7 @@ RSpec.describe "govpay test tasks", type: :rake do
     # By default Rails prevents multiple invocations of the same Rake task in succession
     after { rake_task.reenable }
 
-    it { expect { run_rake_task }.not_to raise_error }
+    it { expect { run_rake_task }.to output(/Signature:/).to_stdout }
 
     context "when Rails environment is production" do
       before do
@@ -37,7 +23,7 @@ RSpec.describe "govpay test tasks", type: :rake do
       end
 
       it "aborts with production mode error" do
-        expect { Rake::Task[rake_task].invoke("test_body") }.to raise_error(SystemExit, "The Rails environment is running in production mode!")
+        expect { Rake::Task[rake_task].invoke("test_body") }.to output("/The Rails environment is running in production mode!/").to_stderr
       end
     end
 
@@ -48,7 +34,7 @@ RSpec.describe "govpay test tasks", type: :rake do
 
       context "when body argument is missing" do
         it "aborts with missing body error" do
-          expect { Rake::Task[rake_task].invoke(nil) }.to raise_error(SystemExit, "missing body argument")
+          expect { Rake::Task[rake_task].invoke(nil) }.to output("/missing body argument/").to_stderr
         end
       end
 
@@ -57,35 +43,18 @@ RSpec.describe "govpay test tasks", type: :rake do
         let(:expected_signature) { "ab05f80fb1cc6dc26eeb309b9c82da1517942df5ab81d921190d0284febe8e35" }
 
         it "runs without error" do
-          expect { Rake::Task[rake_task].invoke(body) }.not_to raise_error
+          expect { Rake::Task[rake_task].invoke(body) }.to output(/Signature:/).to_stdout
         end
 
         it "calls DefraRubyGovpay::WebhookSignatureService with the correct body" do
           allow(DefraRubyGovpay::WebhookSignatureService).to receive(:run)
-          Rake::Task[rake_task].invoke(body)
+          expect { Rake::Task[rake_task].invoke(body) }.to output(/Signature:/).to_stdout
           expect(DefraRubyGovpay::WebhookSignatureService).to have_received(:run).with(body: body)
         end
 
         it "generates a valid signature" do
-          allow(DefraRubyGovpay.configuration).to receive(:front_office_webhook_signing_secret).and_return("foo")
-
-          # Capture output when running the rake task
-          output = StringIO.new
-          original_stdout = $stdout
-          $stdout = output
-
-          # Run the rake task
-          Rake::Task[rake_task].invoke(body)
-
-          # Restore stdout and get the output
-          $stdout = original_stdout
-          captured_output = output.string
-
-          Rails.logger.debug { "Captured output: #{captured_output}" }
-
-          # Extract signature from output
-          signature_match = captured_output.match(/:front_office=>\s*"(.*?)"/)[1]
-          expect(signature_match).to eq(expected_signature)
+          allow(DefraRubyGovpay.configuration).to receive_messages(front_office_webhook_signing_secret: "foo", back_office_webhook_signing_secret: "foo")
+          expect { Rake::Task[rake_task].invoke(body) }.to output(/#{expected_signature}/).to_stdout
         end
       end
     end
