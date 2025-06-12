17a60d1be0840bd28d379d1f573e737b8af29a4c
Leonid Brujev <brujev@gmail.com>
2025-06-12 13:53:47 +0100

    [RUBY-3820] Fixing typo

--- spec/lib/tasks/users_spec.rb
+++ spec/lib/tasks/users_spec.rb
@@ -21,12 +21,12 @@ RSpec.describe "users:deactivate_inactive_users", type: :rake do
         active_user.update(last_sign_in_at: nil)
       end
 
-      it "deactivates the user if the user has been invited more than 3 moths ago" do
+      it "deactivates the user if the user has been invited more than 3 months ago" do
         active_user.update(invitation_sent_at: 1.year.ago)
         expect { rake_task.invoke }.to change { active_user.reload.active }.to(false)
       end
 
-      it "does not deactivate the user if the user has been invited less than 3 moths ago" do
+      it "does not deactivate the user if the user has been invited less than 3 months ago" do
         active_user.update(invitation_sent_at: 1.day.ago)
         expect { rake_task.invoke }.not_to change { active_user.reload.active }
       end
