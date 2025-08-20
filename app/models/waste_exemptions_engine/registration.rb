306fd55410af3a6d16f79ea53097e491490a3d6a
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-08-20 14:35:03 +0100

    Add eligible_for_free_renewal method to registration model

--- app/models/waste_exemptions_engine/registration.rb
+++ app/models/waste_exemptions_engine/registration.rb
@@ -43,6 +43,10 @@ module WasteExemptionsEngine
       state == "expired"
     end
 
+    def eligible_for_free_renewal?
+      business_type == "charity" || registration_exemptions.any? { |re| re.exemption.code == "T28" }
+    end
+
     # rubocop:disable Metrics/CyclomaticComplexity
     def state
       raise "A Registration must have at least one RegistrationExemption." if registration_exemptions.empty?
