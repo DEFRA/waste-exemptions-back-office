09effc75740043bd0bb5ba24110f2eb8ea25bba6
Leonid Brujev <brujev@gmail.com>
2025-09-15 16:36:05 +0100

    [RUBY-3978] Adding task to fix stuck user accounts with expired invitations

--- spec/lib/tasks/users_spec.rb
+++ spec/lib/tasks/users_spec.rb
@@ -2,88 +2,185 @@
 
 require "rails_helper"
 
-RSpec.describe "users:deactivate_inactive_users", type: :rake do
+RSpec.describe "Users tasks", type: :rake do
   include_context "rake"
-  let(:rake_task) { Rake::Task["users:deactivate_inactive_users"] }
-  let(:active_user) { create(:user, active: true, role:) }
 
-  after { rake_task.reenable }
+  describe "users:deactivate_inactive_users" do
+    let(:rake_task) { Rake::Task["users:deactivate_inactive_users"] }
+    let(:active_user) { create(:user, active: true, role:) }
 
-  it { expect { rake_task.invoke }.not_to raise_error }
+    after { rake_task.reenable }
 
-  context "when the user has not signed in for over 3 months" do
-    let(:role) { "data_viewer" }
+    it { expect { rake_task.invoke }.not_to raise_error }
 
-    before { active_user.update(last_sign_in_at: 3.months.ago) }
+    context "when the user has not signed in for over 3 months" do
+      let(:role) { "data_viewer" }
 
-    context "when the user has no last logged in timestamp" do
+      before { active_user.update(last_sign_in_at: 3.months.ago) }
+
+      context "when the user has no last logged in timestamp" do
+        before do
+          active_user.update(last_sign_in_at: nil)
+        end
+
+        it "deactivates the user if the user has been invited more than 3 months ago" do
+          active_user.update(invitation_sent_at: 1.year.ago)
+          expect { rake_task.invoke }.to change { active_user.reload.active }.to(false)
+        end
+
+        it "does not deactivate the user if the user has been invited less than 3 months ago" do
+          active_user.update(invitation_sent_at: 1.day.ago)
+          expect { rake_task.invoke }.not_to change { active_user.reload.active }
+        end
+      end
+
+      context "when the user has last logged in timestamp" do
+        before do
+          active_user.update(last_sign_in_at: 1.year.ago)
+        end
+
+        it "deactivates the user" do
+          expect { rake_task.invoke }.to change { active_user.reload.active }.to(false)
+        end
+      end
+
+      context "when the user is a data_viewer" do
+        it "deactivates the user" do
+          expect { rake_task.invoke }.to change { active_user.reload.active }.to(false)
+        end
+      end
+
+      context "when the user is an admin_team_user" do
+        let(:role) { "admin_team_user" }
+
+        it "does not deactivate the user" do
+          expect { rake_task.invoke }.not_to change { active_user.reload.active }
+        end
+      end
+
+      context "when the user is a developer" do
+        let(:role) { "developer" }
+
+        it "does not deactivate the user" do
+          expect { rake_task.invoke }.not_to change { active_user.reload.active }
+        end
+      end
+    end
+
+    context "when the user has signed in within the last 3 months" do
+      let(:role) { "data_viewer" }
+
       before do
-        active_user.update(last_sign_in_at: nil)
+        active_user.update(last_sign_in_at: 1.month.ago, role:)
       end
-
-      it "deactivates the user if the user has been invited more than 3 months ago" do
-        active_user.update(invitation_sent_at: 1.year.ago)
-        expect { rake_task.invoke }.to change { active_user.reload.active }.to(false)
-      end
-
-      it "does not deactivate the user if the user has been invited less than 3 months ago" do
-        active_user.update(invitation_sent_at: 1.day.ago)
-        expect { rake_task.invoke }.not_to change { active_user.reload.active }
-      end
-    end
-
-    context "when the user has last logged in timestamp" do
-      before do
-        active_user.update(last_sign_in_at: 1.year.ago)
-      end
-
-      it "deactivates the user" do
-        expect { rake_task.invoke }.to change { active_user.reload.active }.to(false)
-      end
-    end
-
-    context "when the user is a data_viewer" do
-      it "deactivates the user" do
-        expect { rake_task.invoke }.to change { active_user.reload.active }.to(false)
-      end
-    end
-
-    context "when the user is an admin_team_user" do
-      let(:role) { "admin_team_user" }
 
       it "does not deactivate the user" do
         expect { rake_task.invoke }.not_to change { active_user.reload.active }
       end
     end
 
-    context "when the user is a developer" do
-      let(:role) { "developer" }
+    describe "dry run mode" do
+      let(:role) { "data_viewer" }
+
+      before { active_user.update(last_sign_in_at: 3.months.ago) }
 
       it "does not deactivate the user" do
-        expect { rake_task.invoke }.not_to change { active_user.reload.active }
+        expect { rake_task.invoke("dry_run") }.not_to change { active_user.reload.active }
       end
     end
   end
 
-  context "when the user has signed in within the last 3 months" do
-    let(:role) { "data_viewer" }
+  describe "users:fix_signed_in_accounts_with_expired_invitations" do
+    let(:rake_task) { Rake::Task["users:fix_signed_in_accounts_with_expired_invitations"] }
 
-    before do
-      active_user.update(last_sign_in_at: 1.month.ago, role:)
+    after { rake_task.reenable }
+
+    it { expect { rake_task.invoke }.not_to raise_error }
+
+    shared_examples "it does not clear the invitation tokens" do
+      it "does not clear the invitation tokens" do
+        user
+        expect { rake_task.invoke }.not_to change { user.reload.invitation_token }
+      end
     end
 
-    it "does not deactivate the user" do
-      expect { rake_task.invoke }.not_to change { active_user.reload.active }
+    context "when user has signed in but has expired invitation" do
+      let(:user) do
+        create(:user,
+               active: true,
+               sign_in_count: 1,
+               invitation_accepted_at: nil,
+               invitation_sent_at: 3.weeks.ago,
+               invitation_token: "sample_token",
+               invitation_created_at: 8.days.ago)
+      end
+
+      it "clears the invitation tokens" do
+        user
+        expect { rake_task.invoke }.to change { user.reload.invitation_token }.from("sample_token").to(nil)
+      end
+
+      it "clears the invitation_created_at" do
+        user
+        expect { rake_task.invoke }.to change { user.reload.invitation_created_at }.to(nil)
+      end
+
+      it "clears the invitation_sent_at" do
+        user
+        expect { rake_task.invoke }.to change { user.reload.invitation_sent_at }.to(nil)
+      end
     end
-  end
 
-  describe "dry run mode" do
-    let(:role) { "data_viewer" }
+    context "when user has not signed in" do
+      let(:user) do
+        create(:user,
+               active: true,
+               sign_in_count: 0,
+               invitation_accepted_at: nil,
+               invitation_sent_at: 8.days.ago,
+               invitation_token: "sample_token")
+      end
 
-    before { active_user.update(last_sign_in_at: 3.months.ago) }
+      it_behaves_like "it does not clear the invitation tokens"
+    end
 
-    it "does not deactivate the user" do
-      expect { rake_task.invoke("dry_run") }.not_to change { active_user.reload.active }
+    context "when invitation is not expired" do
+      let(:user) do
+        create(:user,
+               active: true,
+               sign_in_count: 1,
+               invitation_accepted_at: nil,
+               invitation_sent_at: 5.days.ago,
+               invitation_token: "sample_token")
+      end
+
+      it_behaves_like "it does not clear the invitation tokens"
+    end
+
+    context "when user is inactive" do
+      let(:user) do
+        create(:user,
+               active: false,
+               sign_in_count: 1,
+               invitation_accepted_at: nil,
+               invitation_sent_at: 8.days.ago,
+               invitation_token: "sample_token")
+      end
+
+      it_behaves_like "it does not clear the invitation tokens"
+    end
+
+    context "when invitation has been accepted" do
+      let(:user) do
+        create(:user,
+               active: true,
+               sign_in_count: 1,
+               invitation_accepted_at: 1.day.ago,
+               invitation_sent_at: 8.days.ago,
+               invitation_token: "sample_token")
+      end
+
+      it_behaves_like "it does not clear the invitation tokens"
     end
   end
 end
