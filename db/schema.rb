a15c832331323f36b782f8ad07d8b7dbe5e25b8f
Leonid Brujev <brujev@gmail.com>
2026-02-25 17:42:57 +0000

    [RUBY-4116] Add devise-security session_limitable to restrict simultaneous logins
    When a user signs in from a second browser, the first session is  invalidated and the user is redirected to the sign-in page with an error message.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_12_05_130000) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_25_172751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -500,6 +500,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_12_05_130000) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "unique_session_id", limit: 20
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
