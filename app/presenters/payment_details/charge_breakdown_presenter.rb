1955a6b3b186d0b9f5ef0a254a2d5c830621fdd5
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2026-04-20 20:04:52 +0100

    [RUBY-4189] Add charge breakdown cell styles and update presenter class names (#2069)
    * [RUBY-4189] Add charge breakdown cell styles and update presenter class names

--- app/presenters/payment_details/charge_breakdown_presenter.rb
+++ app/presenters/payment_details/charge_breakdown_presenter.rb
@@ -13,10 +13,16 @@ module PaymentDetails
       end
 
       def breakdown_cell_classes
-        return "govuk-table__cell govuk-!-padding-top-0" if @total
-        return "govuk-!-padding-top-2" if @top_padded
+        classes = ["charge-breakdown__breakdown-cell"]
 
-        nil
+        if @total
+          classes << "govuk-table__cell"
+          classes << "govuk-!-padding-top-0"
+        elsif @top_padded
+          classes << "govuk-!-padding-top-2"
+        end
+
+        classes.join(" ")
       end
 
       def amount_cell_classes
