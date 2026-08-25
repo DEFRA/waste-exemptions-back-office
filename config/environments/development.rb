51d437bb7d9dbe7b22a21dc464f60a0a52388505
Leonid Brujev <brujev@gmail.com>
2026-08-25 15:59:07 +0100

    Disable Rails/StrongParametersExpect cop and clean up development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = false
 
-
   # Raises error for missing translations
   config.i18n.raise_on_missing_translations = true
 
