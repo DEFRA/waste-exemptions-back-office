68b199bee05d1624cde23f6a147e5e9ac5b29f29
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2026-09-08 09:52:31 +0100

    Silence legacy Sass import warnings

--- config/application.rb
+++ config/application.rb
@@ -113,6 +113,7 @@ module WasteExemptionsBackOffice
     # SassC::SyntaxError: Error: "calc(0px)" is not a number for `max'
     # https://github.com/alphagov/govuk-frontend/issues/1350
     config.assets.css_compressor = nil
+    config.sass.silence_deprecations = ["import"]
 
     # Allow paper_trail to deserialise dates and times: https://stackoverflow.com/a/72970171
     config.active_record.yaml_column_permitted_classes = [
